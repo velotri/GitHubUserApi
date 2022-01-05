a83eaf28fec345a5fc3e60fa195a6fca81e2a7ff
velotri <velotril@gmail.com>
2022-01-05 13:53:00 +0100

    Add documentation comments.

--- UserService.cs
+++ UserService.cs
@@ -5,6 +5,9 @@ namespace GitHubUserApi
     public record UserRepository(string Name, string[] Languages);
     public record UserLanguage(string Name, double Percentage);
 
+    /// <summary>
+    /// A service for fetching information about repositories and languages of GitHub users.
+    /// </summary>
     public class UserService
     {
         private record UserData(UserRepository[] UserRepositories, UserLanguage[] UserLanguages, DateTime CacheExpiration);
@@ -25,6 +28,17 @@ namespace GitHubUserApi
             userDataCache = new Dictionary<string, UserData>();
         }
 
+        /// <summary>
+        /// Gets all repositories of the specified user.
+        /// </summary>
+        /// <remarks>
+        /// Results are cached for configurable amount of time. If cache is expired or does not exists, new data is fetched from GitHub API.
+        /// </remarks>
+        /// <returns>
+        /// Repository names and languages used in a given repository.
+        /// </returns>
+        /// <exception cref="RateLimitExceededException"></exception>
+        /// <exception cref="NotFoundException"></exception>
         public async Task<UserRepository[]> GetUserRepositories(string user)
         {
             await FetchUserDataIfNeeded(user);
@@ -32,6 +46,17 @@ namespace GitHubUserApi
             return userDataCache[user].UserRepositories;
         }
 
+        /// <summary>
+        /// Gets all languages used by the specified user. 
+        /// </summary>
+        /// <remarks>
+        /// Results are cached for configurable amount of time. If cache is expired or does not exists, new data is fetched from GitHub API.
+        /// </remarks>
+        /// <returns>
+        /// Language names and their percentage share in total bytes of all repositories of the specified user.
+        /// </returns>
+        /// <exception cref="RateLimitExceededException"></exception>
+        /// <exception cref="NotFoundException"></exception>
         public async Task<UserLanguage[]> GetUserLanguages(string user)
         {
             await FetchUserDataIfNeeded(user);
